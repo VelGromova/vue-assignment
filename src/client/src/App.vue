90a778f74eab99689630ac534a5339d6e23b8805
Valeria Gromova <valeria.gromova@valtech.com>
2021-09-01 21:09:33 +0200

    frontend setup, get websocket data from server, parsed the gps coordinates and pass it to the created MapContainer component

---
+++ src/client/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1>Vehicle data</h1>
+    <div class="map-container">
+      <MapContainer :coordinates="parsedCoordinates"/>
+    </div>
+    <div class="progress-bar">
+      <h3>Current speed: </h3>
+      <span>{{ vehicleData.speed }} km/h</span>
+    </div>
+    <div class="progress-bar">
+      <h3>State of charge: </h3>
+      <span>{{ vehicleData.soc }} %</span>
+    </div>
+    <div class="indicator-list">
+      <div class="indicator-list__item">
+        <span>Energy</span> {{ vehicleData.energy }}
+      </div>
+      <div class="indicator-list__item">
+        <span>Odometer</span> {{ vehicleData.odo }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MapContainer from './components/MapContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MapContainer
+  },
+
+  data() {
+    return {
+      connection: null,
+      vehicleData: {
+        speed: 0,
+        soc: 0,
+      },
+    }
+  },
+
+  computed: {
+    parsedCoordinates() {
+      if (this.vehicleData.gps) {
+        const [lat, lng] = this.vehicleData.gps.split('|').map(parseFloat);
+        return {lat, lng};
+      }
+      return { lat: 52.09126663208008, lng: 5.122183322906494};
+    }
+  },
+
+  created() {
+    this.connection = new WebSocket('ws://localhost:3000/')
+
+    this.connection.onmessage = (event) => {
+      // console.log(event)
+      this.vehicleData = JSON.parse(event.data);
+    }
+
+    this.connection.onopen = (event) => {
+        console.log(event)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.map-container {
+  height: 500px;
+  width: 50%;
+}
+
+</style>
---
+++ src/client/src/components/MapContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="mapContainer" style="height: 100%" />
+</template>
+
+<script> 
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+export default {
+    name: 'MapContainer',
+    props: {
+        coordinates: {
+            type: Object,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            map: null,
+        }
+    },
+    mounted() {
+        this.map = L.map('mapContainer').setView(new L.LatLng(this.coordinates.lat, this.coordinates.lng), 20);
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
+}
+
+</script>
